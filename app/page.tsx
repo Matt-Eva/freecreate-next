35876c031bb535baa2b1e57e97b9e02628c9a5ed
Matt Eva <matteweva@gmail.com>
2025-01-26 19:22:49 -0800

    added postgres connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import connectMongoose from "@/mongo/config";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { createClient } from "@/utils/supabase/server";
+import sql from "@/utils/supabase/pg";
 
 export default async function Home() {
   try {
@@ -11,13 +9,8 @@ export default async function Home() {
     console.error(e);
   }
 
-  return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+  const users = await sql`SELECT id FROM auth.users`;
+  console.log(users);
+
+  return <></>;
 }
