3faae04a67526706310f98be9d21ae0f3ae1078d
Matt Eva <matteweva@gmail.com>
2025-02-25 07:37:19 -0800

    set up google oauth flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
 import Link from "next/link";
 
-export default async function Home() {
+export default function Home() {
+  const supabase = createClient();
+  const googleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:3000/auth/oauth-callback",
+      },
+    });
+  };
+
   return (
     <>
       <h1>FreeCreate</h1>
+      <button onClick={googleLogin}>log in with google</button>
       <Link href="/test">test</Link>
     </>
   );
