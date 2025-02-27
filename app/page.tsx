c1a5dc59c7b8a8dba0f07208ca07d6b2e9757567
Matt Eva <matteweva@gmail.com>
2025-02-27 06:47:41 -0800

    implemented logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 import Link from "next/link";
 
 export default function Home() {
+  const [user, setUser] = useState(false);
+
   const supabase = createClient();
+
   const googleLogin = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
@@ -14,10 +18,36 @@ export default function Home() {
     });
   };
 
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error(error);
+    }
+    setUser(false);
+  };
+
+  const getUser = async () => {
+    const { data, error } = await supabase.auth.getUser();
+    console.log(data);
+    if (data.user) {
+      setUser(true);
+    }
+    // if (error) {
+    //   console.error(error);
+    // }
+  };
+  getUser();
+
   return (
     <>
       <h1>FreeCreate</h1>
-      <button onClick={googleLogin}>log in with google</button>
+      {user ? (
+        <button onClick={logout}>logout</button>
+      ) : (
+        <button onClick={googleLogin}>log in with google</button>
+      )}
+
+      <br />
       <Link href="/test">test</Link>
     </>
   );
