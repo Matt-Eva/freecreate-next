4247c0f125821dc4746716f06516c722c5b86771
Matt Eva <matteweva@gmail.com>
2025-03-16 15:25:48 -0700

    created base auth dialog components

--- app/components/AuthDialog.tsx
+++ app/components/AuthDialog.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
+import LoginForm from "./LoginForm";
+import CreateAccountForm from "./CreateAccountForm";
 import {
   Dialog,
   DialogContent,
@@ -9,6 +15,8 @@ import {
 import { CircleUserRound } from "lucide-react";
 
 function AuthDialog() {
+  const [login, setLogin] = useState(true);
+
   return (
     <Dialog>
       <DialogTrigger className="justify-self-end text-sm sm:text-base ml-auto mr-1 mt-0.5 sm:mr-2 sm:mt-1.5 hover:text-underline text-muted-foreground transition ease-in-out duration-500 hover:text-black">
@@ -19,10 +27,9 @@ function AuthDialog() {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>{login ? "Login" : "Create Account"}</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            {login ? <LoginForm /> : <CreateAccountForm />}
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
