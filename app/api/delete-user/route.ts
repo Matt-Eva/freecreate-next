94582b961ba5d45b8e488e9fbd963e38e29dc6b6
Matt Eva <matteweva@gmail.com>
2025-04-09 16:51:46 -0700

    successfully deleting user

--- app/api/delete-user/route.ts
+++ app/api/delete-user/route.ts
@@ -9,6 +9,13 @@ export async function DELETE(request: Request) {
   } else if (data.user) {
     const user = data.user;
     console.log("user", user);
+    const { data: res, error: err } = await supabase.auth.admin.deleteUser(
+      user.id
+    );
+    if (err) {
+      console.error(err);
+      return new Response(err.message, { status: 500 });
+    } else console.log(res);
   }
 
   return new Response("", { status: 201 });
